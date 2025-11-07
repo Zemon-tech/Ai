c3c3d2b6d01ff569f67db8e1b267405f3e0024a2
ShivangKandoi <shivangkandoi006@gmail.com>
2025-11-07 20:25:48 +0530

    did some UI UX

--- frontend/src/components/ai-elements/message.tsx
+++ frontend/src/components/ai-elements/message.tsx
@@ -24,17 +24,17 @@ export const Message = ({ className, from, ...props }: MessageProps) => (
 );
 
 const messageContentVariants = cva(
-  "is-user:dark flex flex-col gap-2 overflow-hidden rounded-lg text-sm",
+  "is-user:dark flex flex-col gap-2 overflow-hidden rounded-lg text-base font-medium",
   {
     variants: {
       variant: {
         contained: [
-          "max-w-[80%] px-4 py-3",
+          "max-w-[80%] px-3 py-1",
           "group-[.is-user]:bg-primary group-[.is-user]:text-primary-foreground",
           "group-[.is-assistant]:bg-secondary group-[.is-assistant]:text-foreground",
         ],
         flat: [
-          "group-[.is-user]:max-w-[80%] group-[.is-user]:bg-secondary group-[.is-user]:px-4 group-[.is-user]:py-3 group-[.is-user]:text-foreground",
+          "group-[.is-user]:max-w-[80%] group-[.is-user]:bg-secondary group-[.is-user]:px-4 group-[.is-user]:py-2 group-[.is-user]:text-foreground",
           "group-[.is-assistant]:text-foreground",
         ],
       },
